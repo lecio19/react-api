44df96d09f4d63e4d0b15ecb7edaf3e9b8cd7db4
lecio19 <alex.macri01@gmail.com>
2024-12-04 17:33:54 +0100

    add main part

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
-
+import {Header} from "./src/components/header.jsx"
+import {Footer} from "./src/components/Footer.jsx"
+import {Card} from "./src/components/Card.jsx"
 function App() {
   
 
   return (
     <>
-      
+     <Header />
+      <Card />
+      <Footer /> 
     </>
   )
 }
